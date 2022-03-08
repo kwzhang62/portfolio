0ac447f021fc98f652d7124dc7008003df4895aa
KevZ <95584217+kwzhang62@users.noreply.github.com>
2022-03-08 05:42:53 -0500

    added a shuffle feature for the skills section

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,40 @@
 app = {};
 
+app.skillItems = [];
+app.shuffledSkills = [];
+
+//shuffles an array into a random order
+app.shuffle = function(array) {
+    for(let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+app.populateSkillsArray = function() {
+    app.skillItems = document.querySelectorAll('.skillItem');
+    for (let i = 0; i < app.skillItems.length; i++) {
+        app.shuffledSkills.push(app.skillItems[i]);
+    }
+};
+
+app.displaySkills = function(skillsArray) {
+    const skillsList = document.querySelector('#skillsList');
+    for (let i = 0; i < skillsArray.length; i++) {
+        skillsList.appendChild(skillsArray[i]);
+    }
+}
+
+app.setShuffleListener = function() {
+    const shuffleButton = document.querySelector('#shuffle');
+    shuffleButton.addEventListener('click', () => {
+        app.shuffle(app.shuffledSkills);
+        app.displaySkills(app.shuffledSkills);
+    });
+};
+
 app.setSubmitListener = function() {
-    const submitButton = document.querySelector('.submitButton');
+    const submitButton = document.querySelector('#submit');
     const nameField = document.querySelector('#name');
     const emailField = document.querySelector('#email');
     const messageField = document.querySelector('#message')
@@ -16,11 +49,13 @@ app.setSubmitListener = function() {
             emailField.value = '';
             messageField.value = '';
         }
-    })
-}
+    });
+};
 
 app.init = function() {
     app.setSubmitListener();
-}
+    app.populateSkillsArray();
+    app.setShuffleListener();
+};
 
 app.init();
