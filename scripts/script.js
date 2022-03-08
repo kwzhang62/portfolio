d6c74615dfc8ccc98e1a204ce5d126b34efe9ee9
KevZ <95584217+kwzhang62@users.noreply.github.com>
2022-03-08 04:41:14 -0500

    added media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,26 @@
 app = {};
 
-app.init() = {
+app.setSubmitListener = function() {
+    const submitButton = document.querySelector('.submitButton');
+    const nameField = document.querySelector('#name');
+    const emailField = document.querySelector('#email');
+    const messageField = document.querySelector('#message')
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        if(nameField.value == '' || messageField.value == '') {
+            alert('please enter your name and message')
+        } else {
+            //TODO: change the alert to a pop up
+            alert('the data gremlins have received your message and are delivering it to me post haste!');
+            nameField.value = '';
+            emailField.value = '';
+            messageField.value = '';
+        }
+    })
+}
 
+app.init = function() {
+    app.setSubmitListener();
 }
 
 app.init();
